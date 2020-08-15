6caa35c341d1c152adeb17687a771233c8d59612
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-08-14 23:58:14 -0500

    [Add] setting config module

--- src/config/config.module.ts
+++ src/config/config.module.ts
@@ -1,4 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from "./config.service";
 
-@Module({})
+@Module({
+  providers: [
+    {
+      provide: ConfigService,
+      useValue: new ConfigService()
+    }
+  ],
+  exports: [ConfigService]
+})
 export class ConfigModule {}
