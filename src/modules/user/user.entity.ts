261e07db2c04a6083fcb4cafa80e1e2154b1922e
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-08-15 09:13:41 -0500

    [Fix] type date in createdAt

--- src/modules/user/user.entity.ts
+++ src/modules/user/user.entity.ts
@@ -1,5 +1,6 @@
-import { BaseEntity, Entity, PrimaryGeneratedColumn, Column, OneToOne } from "typeorm";
+import { BaseEntity, Entity, PrimaryGeneratedColumn, Column, OneToOne, ManyToMany, JoinColumn, JoinTable } from "typeorm";
 import { UserDetails } from "./user.details.entity";
+import { Role } from "../role/role.entity";
 
 @Entity('users')
 export class User extends BaseEntity {
@@ -15,13 +16,12 @@ export class User extends BaseEntity {
   @Column({ type: 'varchar', nullable: false })
   password: string
 
-  @OneToOne(type => UserDetails, { cascade: true, nullable: false, eager: true })details: UserDetails
-  
+
   @Column({ type: 'varchar', default: 'ACTIVE', length: 8 })
   status: string
   
   @Column({ type: 'timestamp', name: 'created_at' })
-  createdAt: string
+  createdAt: Date
   
   @Column({ type: 'timestamp', name: 'updated_at' })
   updatedAt: Date
