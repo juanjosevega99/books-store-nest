2049293c71b1e10561885ca50bfe9693283fb4f2
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-08-15 01:46:06 -0500

    [Add] database module import and export

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { databaseProviders } from './database.service';
 
-@Module({})
+@Module({
+  imports: [...databaseProviders],
+  exports: [...databaseProviders]
+})
 export class DatabaseModule {}
