714ced8a82e320e4582671a0b2ab2dc6faedcd55
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-08-16 13:16:32 -0500

    [FIX] new migration correction

---
+++ src/database/migrations/1597601659099-fix_name_detail.ts
@@ -0,0 +1,20 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class fixNameDetail1597601659099 implements MigrationInterface {
+    name = 'fixNameDetail1597601659099'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "user-details" DROP COLUMN "email"`);
+        await queryRunner.query(`ALTER TABLE "user-details" ALTER COLUMN "name" DROP NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "user-details" ALTER COLUMN "created_at" DROP NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "user-details" ALTER COLUMN "updated_at" DROP NOT NULL`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "user-details" ALTER COLUMN "updated_at" SET NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "user-details" ALTER COLUMN "created_at" SET NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "user-details" ALTER COLUMN "name" SET NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "user-details" ADD "email" character varying NOT NULL`);
+    }
+
+}
