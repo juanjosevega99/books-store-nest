2e308236f305cf9dd23a6138f23983ad79176d85
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-08-16 01:01:52 -0500

    [FEATURE] CRUD controler user

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Body, Post, Patch, Delete } from '@nestjs/common';
+import { UserService } from './user.service';
+import { User } from './user.entity';
+import { UserDto } from './dto/user.dto';
 
-@Controller('user')
-export class UserController {}
+@Controller('users')
+export class UserController {
+  constructor(private readonly _userService: UserService) {}
+
+  @Get()
+  async getUser(@Param() id: number): Promise<UserDto> {
+    const user = await this._userService.getUser(id);
+    return user;
+  }
+
+  @Get()
+  async getUsers(): Promise<UserDto[]> {
+    const users = await this._userService.getUsers();
+    return users;
+  }
+
+  @Post()
+  async createUser(@Body() user: User): Promise<UserDto> {
+    const createdUser = await this.createUser(user);
+    return createdUser;
+  }
+
+  @Patch()
+  async updateUser(@Param() id: number, @Body() user: User): Promise<UserDto> {
+    const createdUser = await this.createUser(user);
+    return createdUser;
+  }
+
+  @Delete()
+  async deleteUser(@Param() id: number) {
+    await this._userService.delete(id);
+    return true;
+  }
+}
