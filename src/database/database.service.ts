178ef45bf562c1f1a68cb2c58e6a224f871cce74
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-08-16 08:26:50 -0500

    [FIX] bug ssl and port

--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -10,9 +10,10 @@ export const databaseProviders = [
     inject: [ConfigService],
     async useFactory(config: ConfigService) {
       return {
-        ssl: true,
+        // ssl: true,
         type: 'postgres' as 'postgres',
         host: config.get(Configuration.HOST),
+        port: 5444,
         username: config.get(Configuration.USERNAME),
         password: config.get(Configuration.PASSWORD),
         entities: [__dirname + '/../**/*.entity{.ts,.js}'],
