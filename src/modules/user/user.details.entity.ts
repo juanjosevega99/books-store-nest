65e8e0f4df2e33d69a0bcf478274e5d98505589a
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-08-16 13:13:44 -0500

    [Fix] user details

--- src/modules/user/user.details.entity.ts
+++ src/modules/user/user.details.entity.ts
@@ -1,25 +1,29 @@
-import { BaseEntity, Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from "typeorm";
+import {
+  BaseEntity,
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 @Entity('user-details')
 export class UserDetails extends BaseEntity {
   @PrimaryGeneratedColumn('increment')
-  id: number
+  id: number;
 
-  @Column({ type: 'varchar', length: 50, nullable: false })
-  name: string
-  
+  @Column({ type: 'varchar', length: 50, nullable: true })
+  name: string;
+
   @Column({ type: 'varchar', nullable: true })
-  lastName: string
+  lastName: string;
 
-  @Column({ type: 'varchar', nullable: false })
-  email: string
-  
   @Column({ type: 'varchar', default: 'ACTIVE', length: 8 })
-  status: string
-  
-  @CreateDateColumn({ type: 'timestamp', name: 'created_at' })
-  createdAt: string
-  
-  @UpdateDateColumn({ type: 'timestamp', name: 'updated_at' })
-  updatedAt: Date
+  status: string;
+
+  @CreateDateColumn({ type: 'timestamp', name: 'created_at', nullable: true })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamp', name: 'updated_at', nullable: true })
+  updatedAt: Date;
 }
