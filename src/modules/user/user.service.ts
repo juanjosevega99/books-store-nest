cdb598032b515d422140d97d9db97588f505f8fa
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-08-16 00:46:13 -0500

    [Feature] CRUD service user

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,4 +1,67 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  BadRequestException,
+  NotFoundException,
+} from '@nestjs/common';
+import { UserRepository } from './user.repository';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserDto } from './dto/user.dto';
+import { MapperService } from 'src/shared/mapper.service';
+import { User } from './user.entity';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(UserRepository)
+    private readonly _userRepository: UserRepository,
+    private readonly _mapperService: MapperService,
+  ) {}
+
+  async getUser(id: number): Promise<UserDto> {
+    if (!id) {
+      throw new BadRequestException('Id must be sent');
+    }
+
+    const user = await this._userRepository.findOne(id, {
+      where: { status: 'ACTIVE' },
+    });
+
+    if (!user) {
+      throw new NotFoundException();
+    }
+
+    return this._mapperService.map<User, UserDto>(user, new UserDto());
+  }
+
+  async getUsers(): Promise<UserDto[]> {
+    const users = await this._userRepository.find({
+      where: { status: 'ACTIVE' },
+    });
+
+    return this._mapperService.mapCollection<User, UserDto>(
+      users,
+      new UserDto(),
+    );
+  }
+
+  async create(user: User) {
+    const savedUser: User = await this._userRepository.save(user);
+    return this._mapperService.map<User, UserDto>(savedUser, new UserDto());
+  }
+
+  async update(id: number, user: User): Promise<void> {
+    await this._userRepository.update(id, user);
+  }
+
+  async delete(id: number): Promise<void> {
+    const userExists = await this._userRepository.findOne(id, {
+      where: { status: 'ACTIVE' },
+    });
+
+    if (!userExists) {
+      throw new NotFoundException();
+    }
+
+    await this._userRepository.update(id, { status: 'INACTIVE' });
+  }
+}
