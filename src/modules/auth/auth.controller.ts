f5189eb587dad8f306323c95eb200f8426a85174
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-08-19 11:46:29 -0500

    [FIX] token and signin and signup dto

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -5,7 +5,7 @@ import {
   ValidationPipe,
   Body,
 } from '@nestjs/common';
-import { SignupDto, SigninDto } from './dto';
+import { SignupDto, SigninDto, LoggedInDto } from './dto';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
@@ -14,13 +14,13 @@ export class AuthController {
 
   @Post('/signup')
   @UsePipes(ValidationPipe)
-  async signup(@Body() signupDto: SignupDto): Promise<void> {
+  signup(@Body() signupDto: SignupDto): Promise<void> {
     return this._authService.signup(signupDto);
   }
 
   @Post('/signin')
   @UsePipes(ValidationPipe)
-  async signin(@Body() signinDto: SigninDto) {
+  signin(@Body() signinDto: SigninDto): Promise<LoggedInDto> {
     return this._authService.signin(signinDto);
   }
 }
