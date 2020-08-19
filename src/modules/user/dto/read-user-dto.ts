91e9637dd321d734c22cdcaa52f8247695ff74b8
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-08-19 07:28:09 -0500

    [ADD] dto to show important data

--- src/modules/user/dto/read-user-dto.ts
+++ src/modules/user/dto/read-user-dto.ts
@@ -1,17 +1,27 @@
 import { IsNumber, IsEmail, IsString } from 'class-validator';
 import { ReadUserDetailDto } from './read-user-details-dto';
-import { Type } from 'class-transformer';
+import { Type, Exclude, Expose } from 'class-transformer';
+import { ReadRoleDto } from 'src/modules/dtos';
 
+@Exclude()
 export class ReadUserDto {
+  @Expose()
   @IsNumber()
   readonly id: number;
 
+  @Expose()
   @IsEmail()
   readonly email: number;
 
+  @Expose()
   @IsString()
   readonly username: string;
 
+  @Expose()
   @Type(type => ReadUserDetailDto)
   readonly details: ReadUserDetailDto;
+
+  @Expose()
+  @Type(type => ReadRoleDto)
+  readonly roles: ReadRoleDto[];
 }
