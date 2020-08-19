8d645e39391c0b57989b4477d8d5fb3d39a23613
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-08-19 11:15:42 -0500

    [FEaTURE] crud book service

--- src/modules/book/book.service.ts
+++ src/modules/book/book.service.ts
@@ -2,6 +2,7 @@ import {
   Injectable,
   BadRequestException,
   NotFoundException,
+  UnauthorizedException,
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { BookRepository } from './book.repository';
@@ -9,6 +10,12 @@ import { UserRepository } from '../user/user.repository';
 import { status } from '../../shared/entity-status.num';
 import { Book } from './book.entity';
 import { plainToClass } from 'class-transformer';
+import { ReadBookDto, CreateBookDto, UpdateBookDto } from './dtos';
+import { In } from 'typeorm';
+import { User } from '../user/user.entity';
+import { Role } from '../role/role.entity';
+import { RoleType } from '../role/roletype.enum';
+import { CreateRoleDto } from '../dtos';
 
 @Injectable()
 export class BookService {
@@ -19,7 +26,7 @@ export class BookService {
     private readonly _userRepository: UserRepository,
   ) {}
 
-  async get(bookId: number) {
+  async get(bookId: number): Promise<ReadBookDto> {
     if (bookId) {
       throw new BadRequestException('user id must be sent');
     }
@@ -31,5 +38,119 @@ export class BookService {
     if (!book) {
       throw new NotFoundException('book does not exist');
     }
+
+    return plainToClass(ReadBookDto, book);
+  }
+
+  async getAll(): Promise<ReadBookDto[]> {
+    const books: Book[] = await this._bookRepository.find({
+      where: { status: status.ACTIVE },
+    });
+
+    return books.map(book => plainToClass(ReadBookDto, book));
+  }
+
+  async getBooksByAuthor(authorId: number): Promise<ReadBookDto[]> {
+    if (!authorId) {
+      throw new BadRequestException('id must be sent');
+    }
+
+    const books: Book[] = await this._bookRepository.find({
+      where: { status: status.ACTIVE, authors: In([authorId]) },
+    });
+
+    return books.map(book => plainToClass(ReadBookDto, book));
+  }
+
+  async create(book: Partial<CreateBookDto>): Promise<ReadBookDto> {
+    const authors: User[] = [];
+
+    for (const authorId in book.authors) {
+      const authorExists = await this._userRepository.findOne(authorId, {
+        where: { status: status.ACTIVE },
+      });
+
+      if (!authorExists) {
+        throw new NotFoundException(
+          `There's not an author with this Id: ${authorId}`,
+        );
+      }
+
+      const isAuthor = authorExists.roles.some(
+        (role: Role) => role.name === RoleType.AUTHOR,
+      );
+
+      if (!isAuthor) {
+        throw new UnauthorizedException(
+          `This user ${authorId} is not an author`,
+        );
+      }
+
+      authors.push(authorExists);
+    }
+
+    const savedBook: Book = await this._bookRepository.save({
+      name: book.name,
+      description: book.description,
+      authors,
+    });
+    return plainToClass(ReadBookDto, savedBook);
+  }
+
+  async createByAuthor(book: Partial<CreateRoleDto>, authorId: number) {
+    const author = await this._userRepository.findOne(authorId, {
+      where: { status: status.INACTIVE },
+    });
+
+    const isAuthor = author.roles.some(
+      (role: Role) => role.name === RoleType.AUTHOR,
+    );
+
+    if (!isAuthor) {
+      throw new UnauthorizedException(`This user ${authorId} is not an author`);
+    }
+
+    const savedBook: Book = await this._bookRepository.save({
+      name: book.name,
+      description: book.description,
+      author,
+    });
+
+    return plainToClass(ReadBookDto, savedBook);
+  }
+
+  async update(
+    bookId: number,
+    book: Partial<UpdateBookDto>,
+    authorId: number,
+  ): Promise<ReadBookDto> {
+    const bookExists = await this._bookRepository.findOne(bookId, {
+      where: { status: status.ACTIVE },
+    });
+
+    if (!bookExists) {
+      throw new NotFoundException('This book does not exists');
+    }
+
+    const isOwnBook = bookExists.authors.some(author => author.id === authorId);
+
+    if (!isOwnBook) {
+      throw new UnauthorizedException(`This user isn't the book's author`);
+    }
+
+    const updatedBook = await this._bookRepository.update(bookId, book);
+    return plainToClass(ReadBookDto, updatedBook);
+  }
+
+  async delete(bookId: number): Promise<void> {
+    const bookExists = await this._bookRepository.findOne(bookId, {
+      where: { status: status.ACTIVE },
+    });
+
+    if (!bookExists) {
+      throw new NotFoundException('This book does no exists');
+    }
+
+    await this._bookRepository.update(bookId, { status: status.INACTIVE });
   }
 }
